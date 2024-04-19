6fb166514255ee9960bd3e3b89ea54c4e1084c6d
eskehaack <eskehaack@gmail.com>
2024-04-19 13:05:21 +0200

    implementing sampler, non working v3

--- scaling_model/train_model.py
+++ scaling_model/train_model.py
@@ -31,15 +31,15 @@ def main(cfg):
         PredictionWriter(dataloaders=["train", "val", "test"]),
     ]
     # profiler = PyTorchProfiler(filename="profile_out", profile_memory=True)
-    dm = BaselineDataModule(**cfg.data)
+    dm = BaselineDataModule(sampler=cfg.sampler, **cfg.data)
     model = PaiNNforQM9(**cfg.lightning_model)
     trainer = Trainer(
         callbacks=cb,
         # profiler=profiler,
-        # logger=pl.loggers.WandbLogger(
-        #     config=dict(cfg),
-        #     **cfg.logger,
-        # ),
+        logger=pl.loggers.WandbLogger(
+            config=dict(cfg),
+            **cfg.logger,
+        ),
         **cfg.trainer,
     )
     trainer.fit(model, datamodule=dm)
